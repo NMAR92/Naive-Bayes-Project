ce9739e60108b1e427b59ae6654a9ff17ed13610
Nadia Martin <nadmatin@gmail.com>
2022-07-25 02:53:32 +0000

    second

--- src/app.py
+++ src/app.py
@@ -16,13 +16,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_sta
 #3. Preproccesing and model MB
 clf_3 = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
 clf_3.fit(X_train, y_train)
-pred_3 = clf_3.predict(X_test)
 #4.Randomized search to select hyperparameters
 n_iter_search = 5
 parameters = {'vect__ngram_range': [(1, 1), (1, 2)], 'tfidf__use_idf': (True, False), 'clf__alpha': (1e-2, 1e-3)}
 gs_clf_3 = RandomizedSearchCV(clf_3, parameters, n_iter = n_iter_search)
 gs_clf_3.fit(X_train, y_train)
-pred_3_grid = gs_clf_3.predict(X_test)
 best_model = gs_clf_3.best_estimator_
 #5. Save best model
 pickle.dump(best_model, open('../models/best_model.pickle', 'wb'))
